3fa8d66f1f119a3ede88e6c95860301f437a6027
Heitor0 <101339934+Heitor0@users.noreply.github.com>
2022-05-13 06:13:43 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,8 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if ("" == "") {
-        basic.showIcon(IconNames.Yes)
-        basic.pause(5000)
-        basic.showNumber(3)
-        basic.showNumber(2)
-        basic.showNumber(1)
-        basic.showIcon(IconNames.No)
-        basic.pause(5000)
+    if (receivedNumber == "5") {
+        red()
+    } else if (receivedNumber == "") {
+        green()
     }
 })
 function green () {
@@ -16,12 +12,24 @@ function green () {
     range.showColor(neopixel.colors(NeoPixelColors.Black))
     range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Green))
+    for (let index = 0; index < 10; index++) {
+        basic.showNumber(count)
+        count += 1
+    }
 }
 input.onButtonPressed(Button.A, function () {
     if ("" == "") {
         radio.sendNumber(5)
     }
 })
+function traffic () {
+    basic.pause(5000)
+    green()
+    basic.pause(5000)
+    amber()
+    basic.pause(5000)
+    red()
+}
 function amber () {
     range = strip.range(0, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
@@ -41,27 +49,32 @@ function red () {
     range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
+let distance = 0
+let count = 0
 let range: neopixel.Strip = null
 let strip: neopixel.Strip = null
 basic.showLeds(`
-    # . # . #
-    # . # . #
-    # . # . #
-    # . # . #
-    # . # . #
+    # # . # #
+    # # . # #
+    # # . # #
+    # # . # #
+    # # . # #
     `)
+radio.setGroup(5)
 // Put this to P16 when testing.
 strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
 strip.setBrightness(255)
-radio.setGroup(5)
-while (true) {
-    basic.pause(5000)
-    green()
-    basic.pause(5000)
-    amber()
-    basic.pause(5000)
-    red()
-}
+let pedestrian = 0
 basic.forever(function () {
-	
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    distance = pins.pulseIn(DigitalPin.P1, PulseValue.High) + 58
+    basic.pause(2000)
+    if (distance <= 5) {
+        basic.pause(2000)
+        traffic()
+    }
 })
